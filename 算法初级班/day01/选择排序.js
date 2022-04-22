514d99b8fb4690c5a04522339c163c06a43e9398
陈泽鑫 <mrxinxin@aliyun.com>
2022-04-22 23:45:57 +0800

    feat:初阶算法

---
+++ 算法初级班/day01/选择排序.js
@@ -0,0 +1,21 @@
+/*
+ * @Description: 
+ * @Date: 2022-04-22 23:10:07
+ * @LastEditors: 陈泽鑫
+ * @LastEditTime: 2022-04-22 23:28:05
+ * @FilePath: \algorithm\算法初级班\day01\选择排序.js
+ */
+function sortProcess (arr) {
+  if (arr.length <= 1) return arr
+  for (let i=0; i<arr.length-1; i++) {
+    let minIndex = i
+    for(let j=i+1; j<arr.length; j++){
+      minIndex = arr[minIndex] < arr[j] ? minIndex : j
+    }
+    if (minIndex !== i) {
+      let tem = arr[i]
+      arr[i] = arr[minIndex]
+      arr[minIndex] = tem
+    }
+  }
+}
